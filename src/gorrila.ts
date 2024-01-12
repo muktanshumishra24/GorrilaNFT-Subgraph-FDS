aa7f422db9a17fa14b8408f83f654804d38cdb88
muktanshumishra24 <muktanshumishra24@gmail.com>
2024-01-12 20:19:04 +0530

    FDS Subgraph Implemented

--- src/gorrila.ts
+++ src/gorrila.ts
@@ -1,121 +1,99 @@
-import {
-  AffiliateSell as AffiliateSellEvent,
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  DelegateChanged as DelegateChangedEvent,
-  DelegateVotesChanged as DelegateVotesChangedEvent,
-  Initialized as InitializedEvent,
-  Transfer as TransferEvent
-} from "../generated/gorrila/gorrila"
-import {
-  AffiliateSell,
-  Approval,
-  ApprovalForAll,
-  DelegateChanged,
-  DelegateVotesChanged,
-  Initialized,
-  Transfer
-} from "../generated/schema"
+import { Transfer as TransferEvent } from "../generated/gorrila/gorrila";
+import { Token, TokenMetadata, User } from "../generated/schema";
+import { TokenMetadata as TokenMetadataTemplate } from "../generated/templates";
 
-export function handleAffiliateSell(event: AffiliateSellEvent): void {
-  let entity = new AffiliateSell(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.affiliate = event.params.affiliate
+import { json, Bytes, dataSource, log } from "@graphprotocol/graph-ts";
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleDelegateChanged(event: DelegateChangedEvent): void {
-  let entity = new DelegateChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.delegator = event.params.delegator
-  entity.fromDelegate = event.params.fromDelegate
-  entity.toDelegate = event.params.toDelegate
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleDelegateVotesChanged(
-  event: DelegateVotesChangedEvent
-): void {
-  let entity = new DelegateVotesChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.delegate = event.params.delegate
-  entity.previousBalance = event.params.previousBalance
-  entity.newBalance = event.params.newBalance
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+const ipfsHash = "QmYtkZs7FnuVKNNKRzEJ8diWELCiJwiRv35BkKFy2wjJ1L";
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  let token = Token.load(event.params.tokenId.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    token.owner = event.params.to.toHexString();
+    token.tokenID = event.params.tokenId;
+    token.tokenURI = event.params.tokenId.toString();
 
-  entity.save()
+
+    const ipfsHashUri = ipfsHash + "/" + token.tokenURI + ".json";
+
+    token.ipfsHashURI = ipfsHashUri;
+
+    TokenMetadataTemplate.create(ipfsHashUri);
+  }
+  token.updatedAtTimestamp = event.block.timestamp;
+  token.save();
+
+
+
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
+}
+
+
+
+export function handleMetadata(content: Bytes): void {
+  let tokenMetadata = new TokenMetadata(dataSource.stringParam());
+
+  const value = json.fromBytes(content).toObject();
+
+  if (value) {
+    const image = value.get("image");
+    const name = value.get("name");
+    const attributes = value.get("attributes");
+    const description = value.get("description");
+
+    if (name && image && description && attributes) {
+      tokenMetadata.name = name.toString();
+      tokenMetadata.image = image.toString();
+      tokenMetadata.description = description.toString() ;
+      const attributesArray = attributes.toArray();
+
+      if (attributesArray) {
+        for (let i = 0; i < attributesArray.length; i++) {
+          const attributeObject = attributesArray[i].toObject();
+          const trait_type = attributeObject.get("trait_type");
+          const value = attributeObject.get("value");
+
+          if (trait_type && value) {
+            switch (i) {
+              case 0:
+                tokenMetadata.traitType0 = trait_type.toString();
+                tokenMetadata.value0 = value.toString();
+                break;
+              case 1:
+                tokenMetadata.traitType1 = trait_type.toString();
+                tokenMetadata.value1 = value.toString();
+                break;
+              case 2:
+                tokenMetadata.traitType2 = trait_type.toString();
+                tokenMetadata.value2 = value.toString();
+                break;
+              case 3:
+                tokenMetadata.traitType3 = trait_type.toString();
+                tokenMetadata.value3 = value.toString();
+                break;
+              case 4:
+                tokenMetadata.traitType4 = trait_type.toString();
+                tokenMetadata.value4 = value.toString();
+                break;
+              case 5:
+                tokenMetadata.traitType5 = trait_type.toString();
+                tokenMetadata.value5 = value.toString();
+                break;
+              case 6:
+                tokenMetadata.traitType6 = trait_type.toString();
+                tokenMetadata.value6 = value.toString();
+                break;
+            }
+          }
+        }
+      }
+      tokenMetadata.save();
+    }
+  }
 }
